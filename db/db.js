315dc2d126209ad9d24accbe06e8c0aa4ffa3dd1
Nuelsmall <nuelsmall30@gmail.com>
2023-11-21 07:44:56 +0100

    server set up and mongo connection

---
+++ db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const db = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('mongo connected');
+    } catch (error) {
+        console.log('mongo connection error');
+    }
+}
+
+
+module.exports = {db}
